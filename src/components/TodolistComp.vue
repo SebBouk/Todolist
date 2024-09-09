4e63048e439bc14329869b91db45a2923acb9db9
Sébastien Boukobza <seb.boukobza@gmail.com>
2024-09-09 16:52:25 +0200

    modification

--- src/components/TodolistComp.vue
+++ src/components/TodolistComp.vue
@@ -1,23 +1,31 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 const props = defineProps<{
-  todo: { done: boolean; todo: string }
-}>()
-const emit = defineEmits(['onInput'])
+  todo: { done: boolean; todo: string };
+}>();
+const emit = defineEmits(['onInput']);
 const onInput = (value: boolean) => {
-  console.log('TodoComponent a détecté un changement', value)
-  emit('onInput', { ...props.todo, done: value })
-}
+  console.log('TodoComponent a détecté un changement', value);
+  emit('onInput', { ...props.todo, done: value });
+};
+const editMode = ref(false);
 </script>
 
 <template>
-  <span>
-    <!-- <span v-for="element in monTableau" :key="element.todo"> -->
-    {{ props.todo.todo }}
+  <div v-if="!editMode">
+    <span @click="editMode = !editMode">
+      <!-- <span v-for="element in monTableau" :key="element.todo"> -->
+      {{ props.todo.todo }}
+    </span>
     <input
       type="checkbox"
       :checked="props.todo.done"
       @click="(event: any) => onInput(event.target?.checked)"
     />
     <br />
+  </div>
+  <span v-else>
+    <input type="text" :value="props.todo.todo" />
+    <button type="button" @click="editMode = !editMode">X</button>
   </span>
 </template>
--- src/views/TodolistView.vue
+++ src/views/TodolistView.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import TodolistComp from '@/components/TodolistComp.vue'
-import { ref } from 'vue'
+import TodolistComp from '@/components/TodolistComp.vue';
+import { ref } from 'vue';
 
 const monTableau = ref([
   { todo: 'apprendre Vue Js', done: false },
   { todo: 'apprendre à faire des boucles', done: false },
   { todo: 'apprendre à griller des saucisses', done: true }
-])
+]);
 const onTodoInput = (newTodoValue: any, index: number) => {
-  monTableau.value[index] = newTodoValue
-  console.log('monTableau est mis à jour')
-}
+  monTableau.value[index] = newTodoValue;
+  console.log('monTableau est mis à jour');
+};
 </script>
 
 <template>
