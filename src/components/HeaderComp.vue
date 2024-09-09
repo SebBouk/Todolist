e05b78e89fc906d1cda6eb86cbc8b6ef2d3dc8dc
Sébastien Boukobza <seb.boukobza@gmail.com>
2024-09-09 11:41:06 +0200

    css

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -2,9 +2,22 @@
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/todolist">Todolist</RouterLink>
+      <RouterLink to="/" class="nav">Home</RouterLink>
+      <RouterLink to="/todolist" class="nav">Todolist</RouterLink>
+      <RouterLink to="/about" class="nav">About</RouterLink>
     </nav>
   </header>
 </template>
+<style scoped>
+nav {
+  text-align: center;
+  font-size: xx-large;
+}
+.nav {
+  border: 1px solid;
+  color: black;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-decoration: none;
+}
+</style>
